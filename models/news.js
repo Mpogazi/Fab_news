3c38c6608110eaf892b5ec0aad4be6dd87ecd2a9
Fabrice <fbigabiro@gmail.com>
2018-07-04 16:40:38 -0400

    First commit in the repo

--- models/news.js
+++ models/news.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 var newsSchema = mongoose.Schema({
-	name:{type: String, required: true},
+	name:{type: String, required: true, unique: true},
 	author:{type: String, required: true},
-	description:{type: String, required: true},
+	description:{type: String, required: true, unique: true},
 	url: {type: String, required: true},
 	urlToImage: {type: String, required: true},
 	publishedAt:{type: Date, required: true}
 });
+
+
+
+
 
 var article = mongoose.model('article', newsSchema);
 module.exports = article;
